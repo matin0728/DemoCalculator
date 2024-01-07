3d7cea0d1037864f6199f66fbe68674fe098dafa
matin0728 <matin0728@gmail.com>
2024-01-07 20:47:07 +0800

    Initial Commit

---
+++ DoubleCaculatorDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoubleCaculatorDemo
+//
+//  Created by 马月瑶 on 2024/1/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DoubleCaculatorDemoUITests/DoubleCaculatorDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DoubleCaculatorDemoUITestsLaunchTests.swift
+//  DoubleCaculatorDemoUITests
+//
+//  Created by 马月瑶 on 2024/1/7.
+//
+
+import XCTest
+
+final class DoubleCaculatorDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
